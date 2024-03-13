dd456dcafb37fd20acce5d995b5977321526c6ed
youssef nagib <youssefnagib14@yahoo.com>
2024-03-13 04:12:35 +0200

    yalla

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str
+        if ord(i) < 122 and ord(i) > 97
+        i = chr(ord(str) - 32)
+        print("{}".format(c), end="")
+    print("")
