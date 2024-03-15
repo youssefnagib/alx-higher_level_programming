a751e8e44b57e7d35335b711dfccfd5a18981117
youssef nagib <youssefnagib14@yahoo.com>
2024-03-15 03:06:54 +0200

    yalla

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     num = 0
     if 1 == len(sys.argv):
         print("0 arguments.")
+    elif 2 == len(sys.argv):
+        print("1 argument:")
     else:
         n = len(sys.argv) - 1
         print("{} arguments:".format(n))
-        for i in sys.argv[1:]:
-            num += 1
-            print("{}: {}".format(num, i))
+    for i in sys.argv[1:]:
+        num += 1
+        print("{}: {}".format(num, i))
