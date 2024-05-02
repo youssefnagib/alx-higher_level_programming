8dbd4a098141a8a9403345505a90d1710e3f3648
youssef nagib <youssefnagib14@yahoo.com>
2024-05-02 11:58:58 +0300

    division z error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,4 +7,4 @@ def safe_print_division(a, b):
         result = None
         return None
     finally:
-        print("inside result: {}".format(result))
+        print("Inside result: {}".format(result))
