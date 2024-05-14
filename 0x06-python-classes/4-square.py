8b3886e3b56ca3011b2a8209e886130ac92d7fb3
youssef nagib <youssefnagib14@yahoo.com>
2024-05-14 17:21:24 +0300

    setter&getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self):
+    def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
