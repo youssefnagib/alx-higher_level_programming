5c8cc20377dfbde36f3199700eda8584405f0b1a
youssef nagib <youssefnagib14@yahoo.com>
2024-05-14 10:39:21 +0300

    class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
 
         if type(size) is int:
             if size < 0:
-                raise ValueError
+                raise ValueError('size must be >= 0')
             else:
                 self.__size = size
         else:
