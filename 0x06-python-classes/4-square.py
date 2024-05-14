855d9bdce8a52f157c12185ce6be8f509911d792
youssef nagib <youssefnagib14@yahoo.com>
2024-05-14 16:58:21 +0300

    setter&getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,16 +6,7 @@
 class Square:
     """ini square"""
     def __init__(self, size=0):
-
-        """check int"""
-
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-        else:
-            raise TypeError("size must be an integer")
+        self.__size = size
 
     @property
     def size(self):
