bab3fa46b55ae50dc45046a08e9adb73201aff1b
youssef nagib <youssefnagib14@yahoo.com>
2024-05-14 17:18:51 +0300

    setter&getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,17 +11,17 @@ class Square:
 
     def area(self):
         return (self.__size) ** 2
-    
+
     @property
     def size(self):
         return self.__size
 
     @size.setter
     def size(self):
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-        else:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
