378c7a0c8b215dff7bce5195caf0c91d35a425f7
youssef nagib <youssefnagib14@yahoo.com>
2024-05-14 17:54:32 +0300

    #square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+
+
+class Square:
+    """ini square"""
+    def __init__(self, size=0):
+
+        self.size = size
+
+    def area(self):
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+
+            for row in self.__size:
+                for col in self.__size:
+                    print("#", end="")
+                print("")
