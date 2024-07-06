8ac22441507313b5348afee7d7ce675b918e463a
youssef nagib <youssefnagib14@yahoo.com>
2024-07-06 20:15:32 +0300

    Network

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sH "content" -d "$(cat "$2")" "$1"
+curl -sH "content-Type: application/json" -d "$(cat "$2")" "$1"
