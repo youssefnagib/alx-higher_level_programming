60c5688cb5566e1328e74737d9b703d7c5b7d570
youssef nagib <youssefnagib14@yahoo.com>
2024-07-09 19:33:24 +0300

    Network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('http://alx-intranet.hbtn.io/status') as url:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
         response = url.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))
