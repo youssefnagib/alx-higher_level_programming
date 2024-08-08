d507b7457a08f0e258bcbf431b35e18c3cee4291
youssef nagib <youssefnagib14@yahoo.com>
2024-08-08 05:50:43 +0300

    js

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#add_item').click(function() {
+        let newItem = $('<li>Item</li>');
+        $('ul.my_list').append(newItem);
+    });
+});
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#update_header').click(function() {
+        // Update the text of the <header> element
+        $('header').text('New Header!!!');
+    });
+});
